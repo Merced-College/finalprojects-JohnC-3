4385dfd4965bb73a64ac7115bf392e9edfc5cbbb
John Chiero <john.chiero@campus.mccd.edu>
2026-04-16 23:21:54 +0000

    Created setters and getters for Book and Member classes.

--- Book.java
+++ Book.java
@@ -3,5 +3,57 @@
 // Book class
 
 public class Book {
-    
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+    private String genre;
+
+    public Book(String title, String author, String isbn, int publicationYear, String genre) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
--- Member.java
+++ Member.java
@@ -3,5 +3,118 @@
 // Member class
 
 public class Member {
-    
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String membershipType;
+    private int joinDate;
+    private int expirationDate;
+    private int maxBooks;
+    private int currBooks;
+
+    public Member(int id, String name, String email, String phone, String address, String membershipType, int joinDate, int expirationDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.membershipType = membershipType;
+        this.joinDate = joinDate;
+        this.expirationDate = expirationDate;
+        this.currBooks = 0;
+
+        if(membershipType.equals("Standard")) {
+            this.maxBooks = 2;
+        } else if(membershipType.equals("Student")) {
+            this.maxBooks = 3;
+        } else if(membershipType.equal("Faculty")) {
+            this.maxBooks = 4;
+        } else if(membershipType.equals("Premium")) {
+            this.maxBooks = 5;
+        } else {
+            this.maxBooks = 0;
+        }
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public int getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(int joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public int getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(int expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getMaxBooks() {
+        return maxBooks;
+    }
+
+    public void setMaxBooks(int maxBooks) {
+        this.maxBooks = maxBooks;
+    }
+
+    public int getCurrBooks() {
+        return currBooks;
+    }
+
+    public void setCurrBooks(int currBooks) {
+        this.currBooks = currBooks;
+    }
 }
