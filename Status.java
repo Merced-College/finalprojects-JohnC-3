2e92fbdb1938aad372aabc343b4f1e37e6b509f1
John Chiero <john.chiero@campus.mccd.edu>
2026-04-18 00:50:21 +0000

    Started to create Status class.

---
+++ Status.java
@@ -0,0 +1,14 @@
+// John Chiero
+// 4/17/2026
+// Status class
+
+public class Status {
+    private String availability;
+    private Member member;
+    private String borrowDate;
+    private String dueDate;
+
+    public Status() {
+        
+    }
+}
